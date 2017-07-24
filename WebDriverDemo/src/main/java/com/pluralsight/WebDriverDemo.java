c579b906a63755f7553a819ebbed283be84946c1
Bryan Hansen <bryankhansen@gmail.com>
2017-07-24 12:01:20 -0600

    Web Driver Initial Script
    Start of the checkin history per branch.

---
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverDemo {
+
+	public static void main(String[] args) {
+		
+		//System.setProperty("webdriver.gecko.driver", "C:/dev/tools/geckodriver.exe");
+		
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		
+		driver.findElement(By.id("lst-ib"));
+		
+	}
+
+}
