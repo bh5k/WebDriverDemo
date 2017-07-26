9517daef3e9870e90a06c9c522840dc6885b99c4
Bryan Hansen <bryankhansen@gmail.com>
2017-07-26 01:22:43 -0600

    Final checkin of m3

--- WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -8,16 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverDemo {
 
 	public static void main(String[] args) {
-		
-		//System.setProperty("webdriver.gecko.driver", "C:/dev/tools/geckodriver.exe");
-		
-		//WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		
 		WebElement searchField = driver.findElement(By.id("lst-ib"));
-		
 		searchField.sendKeys("pluralsight");
+		searchField.submit();
+		
+		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+		imagesLink.click();
+		
+		WebElement imageElement = driver.findElements(By.cssSelector("a[class = rg_l]")).get(0);
+		WebElement imageLink = imageElement.findElements(By.tagName("img")).get(0);
+		imageLink.click();
 	}
-
 }
