da80f7722e60df93e683fd1a4f8a46d1fe606b27
Bryan Hansen <bryankhansen@gmail.com>
2017-08-06 15:27:59 -0600

    m4 final changes

--- WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -1,11 +1,11 @@
 package com.pluralsight;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverDemo {
 
@@ -17,7 +17,9 @@ public class WebDriverDemo {
 		searchField.sendKeys("pluralsight");
 		searchField.submit();
 		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Images")));
 		
 		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
 		imagesLink.click();
