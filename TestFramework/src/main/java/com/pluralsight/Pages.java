67d9f2231944ef0790f29c89b48fa1c8fb6d15f1
Bryan Hansen <bryankhansen@gmail.com>
2017-08-30 20:45:09 -0600

    Demo tests

---
+++ TestFramework/src/main/java/com/pluralsight/Pages.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class Pages {
+
+	public static HomePage homePage() {
+		return new HomePage();
+	}
+}
---
+++ Tests/src/test/java/com/pluralsight/UnitTest.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void canGoToHomePage() {
+		Pages.homePage().goTo();
+		Assert.assertTrue(Pages.homePage().isAt());
+	}
+	
+	@After
+	public void cleanUp() {
+		Browser.close();
+	}
+
+}
